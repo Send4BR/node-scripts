b70588671fe67cc4d09696597acee24a4ad52ae9
Gustavo Santos <gustavofsantos@outlook.com>
2021-10-22 16:07:06 -0300

    feat: add base eslint and prettier config

---
+++ shared/prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: "always",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  endOfLine: "lf",
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "none",
+  useTabs: false
+};
